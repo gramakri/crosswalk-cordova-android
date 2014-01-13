3b994547e45f65ca9e994c5caab109265dfcc14b
Girish Ramakrishnan <girish@forwardbias.in>
2014-01-12 18:12:50 -0800

    Add xwalk_core_library from crosswalk-4.32.69.0-arm

---
+++ framework/xwalk_core_library/src/org/chromium/content/app/NativeLibraries.java
@@ -0,0 +1,13 @@
+
+
+
+
+package org.chromium.content.app;
+
+public class NativeLibraries {
+    public static boolean USE_LINKER = false;
+    public static boolean ENABLE_LINKER_TESTS = false;
+    public static String[] libraries
+= { "xwalkcore"}
+      ;
+}
